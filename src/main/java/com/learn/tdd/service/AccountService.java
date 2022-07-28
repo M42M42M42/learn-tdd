5c7ad1bd61aa3d6b863f0bde821318d2e086d498
zhengxiongli <TW_25>
2022-07-28 13:31:50 +0800

    [story-1] Service：given 一个已经存在的用户名和有效密码 when 注册 then 抛出异常，异常信息<用户名已使用>

--- src/main/java/com/learn/tdd/service/AccountService.java
+++ src/main/java/com/learn/tdd/service/AccountService.java
@@ -11,6 +11,9 @@ public class AccountService {
     private final AccountRepository accountRepository;
 
     public void register(String username, String password) {
+        if (accountRepository.findByUsername(username).isPresent()) {
+            throw new RuntimeException("用户名已使用");
+        }
         accountRepository.save(new Account(username, password));
     }
 }
--- src/test/java/com/learn/tdd/service/AccountServiceTest.java
+++ src/test/java/com/learn/tdd/service/AccountServiceTest.java
@@ -1,12 +1,21 @@
 package com.learn.tdd.service;
 
 import com.learn.tdd.BaseUnitTest;
+import com.learn.tdd.entity.Account;
 import com.learn.tdd.repository.AccountRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
 class AccountServiceTest extends BaseUnitTest {
+    private static final String USERNAME = "TestUser";
+    private static final String PASSWORD = "password001";
     @Mock
     private AccountRepository accountRepository;
     @InjectMocks
@@ -15,6 +24,15 @@ class AccountServiceTest extends BaseUnitTest {
     @Test
     void should_register_success_when_register_given_username_and_password_valid() {
         // given when then
-        accountService.register("TestUser", "password001");
+        accountService.register(USERNAME, PASSWORD);
+    }
+
+    @Test
+    void should_throw_exception_when_register_given_an_exist_username() {
+        // given
+        given(accountRepository.findByUsername(anyString())).willReturn(Optional.of(new Account()));
+
+        // when
+        assertThrows(RuntimeException.class, () -> accountService.register(USERNAME, PASSWORD), "用户名已使用");
     }
 }
