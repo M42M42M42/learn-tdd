487df7a5ba7cbc81debe00b6e453242b74e9ed30
zhengxiongli <TW_25>
2022-07-28 13:40:31 +0800

    [story-1] Controller: given 一个已经存在的用户名和有效密码 when 注册 then 返回<用户名已使用>，httpStatus：400

--- src/test/java/com/learn/tdd/controller/AccountControllerTest.java
+++ src/test/java/com/learn/tdd/controller/AccountControllerTest.java
@@ -4,6 +4,9 @@ import com.learn.tdd.BaseApiTest;
 import com.learn.tdd.controller.request.AccountRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.hamcrest.Matchers.equalTo;
 
 public class AccountControllerTest extends BaseApiTest {
 
@@ -19,5 +22,18 @@ public class AccountControllerTest extends BaseApiTest {
 
         // then
         dbAssertThat("select * from accounts where username = ?", request.getUsername()).hasNumberOfRows(1);
+    }
+
+    @Test
+    @Sql("classpath:sql/insertUserToDb.sql")
+    void should_return_400_when_register_given_exist_username_and_valid_password() {
+        // given
+        AccountRequest request = new AccountRequest();
+        request.setUsername("TestUser");
+        request.setPassword("password001");
+
+        // when
+        given().body(request).post("/accounts/register").then().status(HttpStatus.BAD_REQUEST)
+                .body(equalTo("用户名已使用"));
     }
 }
